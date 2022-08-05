449d3ee853e12b5c7bd702f1f18ac0d58ceddbcd
ignasinou@gmail.com <ignasinou@gmail.com>
2022-08-05 16:33:21 +0200

    Add output video

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import cv2
 import tqdm
 from modules import FaceDetection, IdentityVerification, LivenessDetection
 import argparse
+import subprocess
 
 root = Path(os.path.abspath(__file__)).parent.absolute()
 data_folder = root / "data"
@@ -26,6 +27,13 @@ model_selection = 1
 
 file_extension = "." + args.videoFile.split(".")[-1]
 output_video_filename = args.videoFile.replace(file_extension, '_output.mp4')
+output_audio_filename = args.videoFile.replace(file_extension, '.wav')
+
+subprocess.call(
+    'ffmpeg -hwaccel cuda -i %s -pix_fmt yuv420p -c:v h264_nvenc -cq:a 0 -ac 1 -vn -threads 10 -ar 16000 %s' % (
+        args.videoFile,
+        output_audio_filename),
+    shell=True)
 
 faceDetector = FaceDetection(min_detection_confidence, model_selection)
 identityChecker = IdentityVerification(
@@ -39,8 +47,8 @@ width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fps = video.get(cv2.CAP_PROP_FPS)
 outVideo = cv2.VideoWriter(
-                    output_video_filename
-                    , cv2.VideoWriter_fourcc(*'XVID'), fps, (width, height))
+    output_video_filename
+    , cv2.VideoWriter_fourcc(*'XVID'), fps, (width, height))
 
 while True:
     for frame_num in tqdm.tqdm(range(int(num_frames))):
@@ -64,7 +72,7 @@ while True:
             liveness_score = float(f"{liveness_score:0.5f}")
             frame = cv2.rectangle(frame, box[0], box[1], color, 10)
             cv2.putText(frame, f"{liveness_score}", (box[0][0], box[0][1] + 30), cv2.FONT_HERSHEY_SIMPLEX,
-                       1, (255, 255, 255), 2, cv2.LINE_AA)
+                        1, (255, 255, 255), 2, cv2.LINE_AA)
             outVideo.write(frame.copy())
         if frame_num >= 100:
             break
@@ -73,3 +81,9 @@ while True:
 outVideo.release()
 cv2.destroyAllWindows()
 video.release()
+
+subprocess.call(
+    'ffmpeg -i %s -i %s -shortest  %s -y' % (output_video_filename,
+                                             output_audio_filename,
+                                             output_video_filename.replace(file_extension, "_AV.mp4")),
+    shell=True)
